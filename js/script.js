7b39be87e61c095986f079ecd47cb394bcc700be
Emma Guy <dusk@Vampiredusk.local>
2019-06-05 21:29:17 +1200

    demo works without content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
-  $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 160
-  });
+
+var grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 160,
+  stagger: 30,
+});
+
+//stagger function//
+grid.on('click','.grid-item',function(){
+  $( this ).toggleClass('grid-item--gigante');
+  grid.masonry('layout');
+});
+
+
 });
